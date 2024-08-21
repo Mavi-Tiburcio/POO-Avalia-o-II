0b4e81bb237d0ede311129da37facfd1f15d1d37
Mavi-Tiburcio <mvot@discente.ifpe.edu.br>
2024-08-21 13:44:09 -0300

    Remove pasta Q2

--- Q2 Backup e Restauração/Command.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-
-public interface Command {
-    void execute();
-    void store() throws IOException;
-    void load() throws IOException;
-}
--- Q2 Backup e Restauração/Light.java
+++
@@ -1,9 +0,0 @@
-public class Light {
-    public void on() {
-        System.out.println("Luz ligada.");
-    }
-
-    public void off() {
-        System.out.println("Luz desligada.");
-    }
-}
